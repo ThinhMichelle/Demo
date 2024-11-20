6e13c87432a060164322d04b7eff175f7f295968
ThinhNguyen <96777799+ThinhMichelle@users.noreply.github.com>
2024-11-20 14:23:44 +0700

    Update lan1

--- Demo_First/Demo_First/Properties/Resources_ja-JP.Designer.cs
+++ Demo_First/Demo_First/Properties/Resources_ja-JP.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-using System;
-
-namespace Demo_First.Properties
-{
+namespace Demo_First.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -22,180 +22,148 @@ namespace Demo_First.Properties
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources_ja_JP
-    {
-
+    internal class Resources_ja_JP {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources_ja_JP()
-        {
+        internal Resources_ja_JP() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if (object.ReferenceEquals(resourceMan, null))
-                {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Demo_First.Properties.Resources_ja-JP", typeof(Resources_ja_JP).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to キャンセル.
         /// </summary>
-        internal static string GEN_Cancel
-        {
-            get
-            {
+        internal static string GEN_Cancel {
+            get {
                 return ResourceManager.GetString("GEN_Cancel", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to エラー.
         /// </summary>
-        internal static string GEN_Error
-        {
-            get
-            {
+        internal static string GEN_Error {
+            get {
                 return ResourceManager.GetString("GEN_Error", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to 成功しました.
         /// </summary>
-        internal static string GEN_Finished
-        {
-            get
-            {
+        internal static string GEN_Finished {
+            get {
                 return ResourceManager.GetString("GEN_Finished", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to 情報.
         /// </summary>
-        internal static string GEN_Info
-        {
-            get
-            {
+        internal static string GEN_Info {
+            get {
                 return ResourceManager.GetString("GEN_Info", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to 情報.
         /// </summary>
-        internal static string GEN_Information
-        {
-            get
-            {
+        internal static string GEN_Information {
+            get {
                 return ResourceManager.GetString("GEN_Information", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to 入力値は整数で入力してください。.
         /// </summary>
-        internal static string GEN_InputRequireInteger
-        {
-            get
-            {
+        internal static string GEN_InputRequireInteger {
+            get {
                 return ResourceManager.GetString("GEN_InputRequireInteger", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to 入力値は整数で、{0} 以上で入力してください。.
         /// </summary>
-        internal static string GEN_InputRequireIntegerAndGreaterEqualValue
-        {
-            get
-            {
+        internal static string GEN_InputRequireIntegerAndGreaterEqualValue {
+            get {
                 return ResourceManager.GetString("GEN_InputRequireIntegerAndGreaterEqualValue", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to 入力値を入力してください。.
         /// </summary>
-        internal static string GEN_InputRequireValidation
-        {
-            get
-            {
+        internal static string GEN_InputRequireValidation {
+            get {
                 return ResourceManager.GetString("GEN_InputRequireValidation", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to 入力値が正しくありません。再度確認してください。.
         /// </summary>
-        internal static string GEN_InputValuesIncorrect
-        {
-            get
-            {
+        internal static string GEN_InputValuesIncorrect {
+            get {
                 return ResourceManager.GetString("GEN_InputValuesIncorrect", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to 適用.
         /// </summary>
-        internal static string GEN_Preview
-        {
-            get
-            {
+        internal static string GEN_Preview {
+            get {
                 return ResourceManager.GetString("GEN_Preview", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to 成功しました.
         /// </summary>
-        internal static string GEN_Success
-        {
-            get
-            {
+        internal static string GEN_Success {
+            get {
                 return ResourceManager.GetString("GEN_Success", resourceCulture);
             }
         }
-
+        
         /// <summary>
         ///   Looks up a localized string similar to 警告.
         /// </summary>
-        internal static string GEN_Warning
-        {
-            get
-            {
+        internal static string GEN_Warning {
+            get {
                 return ResourceManager.GetString("GEN_Warning", resourceCulture);
             }
         }
